dedc2c528f3405bbc37b56ce81229b180c6c3276
Sandeeb Adhikari <sa2734@njit.edu>
2024-10-29 22:51:06 -0400

    change functionality on waveText, about and aboutcard

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,22 +1,48 @@
-import { AnimatePresence, motion } from "framer-motion";
+import {
+  AnimatePresence,
+  motion,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 import React from "react";
-
 import MultiDirectionSlide from "../../animations/MultiDirectionSlide";
+import EducationImg from "../../../public/images/education.png";
+import ExperienceImg from "../../../public/images/experience.png";
 import AboutCard from "./AboutCard";
 
 const About: React.FC = () => {
+  const { scrollYProgress } = useScroll();
+
+  const leftCardX = useTransform(scrollYProgress, [0, 0.6], ["-100%", "0%"]);
+  const rightCardX = useTransform(scrollYProgress, [0, 0.6], ["100%", "0%"]);
+
   const description =
     "I am a Full-Stack Developer with experience in building web applications using React, Next.js, and Node.js. I am also a freelance developer and open-source contributor.";
+
   return (
     <AnimatePresence>
       <div className="text-7xl w-full h-[660px]">
         <MultiDirectionSlide text1="About Me" />
-        <div className="text-xl">
+        <div className="text-xl mb-10">
           <MultiDirectionSlide text2="Get To Know More" />
         </div>
+
         <div className="flex justify-between items-center w-full gap-5">
-          <AboutCard title="Experience" description={description} />
-          <AboutCard />
+          <motion.div style={{ x: leftCardX }} className="flex-1">
+            <AboutCard
+              title="Experience"
+              description={description}
+              image={ExperienceImg}
+            />
+          </motion.div>
+
+          <motion.div style={{ x: rightCardX }} className="flex-1">
+            <AboutCard
+              title="Education"
+              description={description}
+              image={EducationImg}
+            />
+          </motion.div>
         </div>
       </div>
     </AnimatePresence>
--- src/components/About/AboutCard.tsx
+++ src/components/About/AboutCard.tsx
@@ -3,14 +3,19 @@ import WavyText from "../../animations/WavyText";
 interface AboutCardProps {
   title: string;
   description: string;
+  image: string;
 }
 
-const AboutCard: React.FC<AboutCardProps> = ({ title, description }) => {
+const AboutCard: React.FC<AboutCardProps> = ({ title, description, image }) => {
   return (
     <div className="p-5 border w-full h-[200px] rounded-3xl">
-      <h1 className="text-4xl">
-        <WavyText word={title} />
-      </h1>
+      <div className="flex justify-center gap-2">
+        <img src={image} className="w-8 h-8 rounded-full" />
+        <h1 className="text-4xl">
+          <WavyText word={title} />
+        </h1>
+      </div>
+
       <p className="text-lg">{description}</p>
     </div>
   );
